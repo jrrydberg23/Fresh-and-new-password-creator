a21ca820ae3ee0db95315ffe9688c10d6c5efb86
James Rydberg <jrrydberg23@gmail.com>
2021-05-22 21:55:29 -0500

    Finished prompt functions.

--- Develop/script.js
+++ Develop/script.js
@@ -15,23 +15,59 @@ var specialChar = ["!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","
 // Function for password length
 function length() {
   passwordLength = prompt("Enter how many characters you want in your password.(Minimum 8, Max 128.)")
+  if (passwordLength < 8) {
+    passwordLength = false;
+    return passwordLength;
+  } else if (passwordLength > 128) {
+    passwordLength = false;
+    return passwordLength;
+  } else if (passwordLength > 8) {
+    passwordLength = true;
+  } else (passwordLength < 128); {
+    passwordLength = true;
+  }
+  return passwordLength
 }
 
 // Function for lowercase
 function lowercase() {
   lowercaseChoice = prompt("Do you want lowercase letters in your password? \n(Yes or No)")
+  if (lowercaseChoice === 'yes'){
+    lowercaseChoice = true;
+  } else (lowercaseChoice === 'no');{
+    lowercaseChoice = false;
+  }
+  return lowercaseChoice
 }
 // Function for Uppercase
 function uppercase() {
   uppercaseChoice = prompt("Do you want uppercase letters in your password? \n(Yes or No)")
+  if (uppercaseChoice === 'yes'){
+    uppercaseChoice = true;
+  } else (upperaseChoice === 'no');{
+    uppercaseChoice = false;
+  }
+  return uppercaseChoice
 }
 // Function for NumericChar
 function numeric() {
   numericChoice = prompt("Do you want numbers in your password? \n(Yes or No)")
+  if (numericChoice === 'yes'){
+    numericChoice = true;
+  } else (numericChoice === 'no');{
+    numericChoice = false;
+  }
+  return numericChoice
 }
 // Function for specialChar
 function special() {
   specialChoice = prompt("Do you want special characters in your password? \n(Yes or No)")
+  if (specialChoice === 'yes'){
+    specialChoice = true;
+  } else (specialChoice === 'no');{
+    specialChoice = false;
+  }
+  return specialChoice
 }
 
 // Function to generate password
